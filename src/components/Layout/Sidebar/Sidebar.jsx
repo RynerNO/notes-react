72d05142b8f47e287202534ad815efe43f4c87da
RynerNO <ryner.no@gmail.com>
2023-05-11 19:04:32 +0200

    finished styling and some of the event hadling on sidebar and listitem components

--- src/components/Layout/Sidebar/Sidebar.jsx
+++ src/components/Layout/Sidebar/Sidebar.jsx
@@ -6,16 +6,15 @@ import ListItem from "../../ListItem/ListItem";
 
 function Sidebar() {
   const { data, selectedNote, updateValue } = useContext(NotesContext);
-  const selectNote = () => {
-    console.log(data);
-    updateValue({ selectedNote: 2 });
+  const selectNote = (index) => {
+    updateValue({ selectedNote: index });
   };
   return (
     <div className={styles.sidebar}>
       <ul>
         {data.map((item, index) => {
           return (
-            <li key={item.id} onClick={selectNote}>
+            <li key={item.id} onClick={() => selectNote(index)}>
               <ListItem
                 title={item.values[FIELDS.title]}
                 text={item.values[FIELDS.text]}
